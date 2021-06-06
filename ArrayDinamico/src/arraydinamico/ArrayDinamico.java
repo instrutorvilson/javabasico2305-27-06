31838a0ac69a1739e2d222d03a0f39326d8a07e4
vilson.moro <vilson.moro@RED00.proway.treina>
2021-06-06 16:06:30 -0300

    Adicionar item ao vetor sem metodos

--- ArrayDinamico/src/arraydinamico/ArrayDinamico.java
+++ ArrayDinamico/src/arraydinamico/ArrayDinamico.java
@@ -1,22 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package arraydinamico;
 
 import java.util.Scanner;
-
-/**
- *
- * @author vilson.moro
- */
 public class ArrayDinamico {
-
-    static String[] nomes = new String[1];
-
+ 
+    
     public static void main(String[] args) {
-
+        String[] nomes = new String[1];
         boolean continuar = true;
         int i = 0;
         while (continuar) {
@@ -24,30 +14,24 @@ public class ArrayDinamico {
             nomes[i] = new Scanner(System.in).nextLine();
             System.out.println("Deseja continuar? [s/n]");
             String opcao = new Scanner(System.in).nextLine();
-            continuar = opcao.equalsIgnoreCase("s");  
-            if (continuar){
-                add();
-                i++;             
+            continuar = opcao.equalsIgnoreCase("s");
+            if (continuar) {
+                //add();
+                String[] aux = new String[nomes.length];
+                for (int x = 0; x < nomes.length; x++) {
+                    aux[x] = nomes[x];
+                }
+                nomes = new String[aux.length + 1];
+                for (int x = 0; x < aux.length; x++) {
+                    nomes[x] = aux[x];
+                }
+                i++;
             }
         }
-        listar();       
-    }
-
-    public static void listar(){
-         //listar nomes informados
-       System.out.println("===============================");
-       for(String nome : nomes){
-           System.out.println(nome);
-       }
-    }
-    public static void add() {
-        String[] aux = new String[nomes.length];
-        for (int x = 0; x < nomes.length; x++) {
-            aux[x] = nomes[x];
-        }
-        nomes = new String[aux.length + 1];
-        for (int x = 0; x < aux.length; x++) {
-            nomes[x] = aux[x];
+       // listar();
+        System.out.println("===============================");
+        for (String nome : nomes) {
+            System.out.println(nome);
         }
     }
 
